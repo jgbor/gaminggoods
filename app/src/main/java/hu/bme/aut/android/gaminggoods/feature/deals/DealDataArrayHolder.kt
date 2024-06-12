9b5eef93d777b9fe441ebcd3830530af764261b8
jgbor <100523032+jgbor@users.noreply.github.com>
2024-06-12 11:42:45 +0200

    reset repo

--- app/src/main/java/hu/bme/aut/android/gaminggoods/feature/deals/DealDataArrayHolder.kt
+++
@@ -1,7 +0,0 @@
-package hu.bme.aut.android.gaminggoods.feature.deals
-
-import hu.bme.aut.android.gaminggoods.model.DealData
-
-interface DealDataArrayHolder {
-    fun getDealDataList(): Array<DealData?>?
-}
